ad18c2c1196c574a929f4e51f69802aa861273db
Jamison Ordway <jamisonordway@gmail.com>
2023-10-12 14:21:58 -0600

    Complete Iteration 1

--- jamison/python/flashcards/cardtest.py
+++ jamison/python/flashcards/cardtest.py
@@ -6,34 +6,9 @@ class CardTests(unittest.TestCase):
   def test_it_exists(self):
         card = Card("What is the capital of Alaska?", "Juneau", "Geography")
         isinstance(card, Card)
+        self.assertEqual(card.question, "What is the capital of Alaska?")
+        self.assertEqual(card.answer, "Juneau")
+        self.assertEqual(card.category, "Geography")
 
 if __name__ == '__main__':
   unittest.main()
-
-  # require './lib/card'
-
-# RSpec.describe Card do
-#   it 'exists' do
-#     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-#     expect(card).to be_instance_of(Card)
-#   end
-
-#   it 'has a question' do
-#     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-#     expect(card.question).to eq("What is the capital of Alaska?")
-#   end
-
-#   it 'has an answer' do
-#     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-#     expect(card.answer).to eq("Juneau")
-#   end
-
-#   it 'has a category' do
-#     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-#     expect(card.category).to eq(:Geography)
-#   end
-# end
