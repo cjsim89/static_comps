5e9bcf5bd9715ef45db9db87b7d86738d8cb368a
Jamison Ordway <jamisonordway@gmail.com>
2024-02-28 11:02:35 -0700

    WIP percent correct by category

--- jamison/python/flashcards/round.py
+++ jamison/python/flashcards/round.py
@@ -5,7 +5,7 @@ class Round:
   def __init__(self, deck):
     self.deck = deck
     self.turns = []
-    self.answered_cards = []
+    # self.answered_cards = []
     self.number_correct = 0
 
   def current_card(self):
@@ -15,7 +15,6 @@ class Round:
     new_turn = Turn(guess, self.current_card())
     if guess == self.current_card().answer:
       self.number_correct += 1
-    self.answered_cards.append(self.current_card())
     self.deck.cards.remove(self.current_card())
     self.turns.append(new_turn)
     return new_turn
@@ -26,7 +25,27 @@ class Round:
     percentage = self.number_correct / len(self.turns)
     return f"{round(percentage, 2)}%"
   
-  def correct_by_category(self, category):
-    # category_cards = self.deck.cards_in_category(category)
+  def turn_categories(self):
+    categories = {}
     for turn in self.turns:
-      print(turn)
+      if categories.get(turn.card.category):
+        categories[turn.card.category] += 1
+      else:
+        categories[turn.card.category] = 1
+    return categories
+  
+  def correct_by_category(self):
+    correct_category_turns = {}
+    for turn in self.turns:
+      if turn.correct() == True:
+        if correct_category_turns.get(turn.card.category):
+          correct_category_turns[turn.card.category] += 1
+        else:
+          correct_category_turns[turn.card.category] = 1
+      else:
+        if not correct_category_turns.get(turn.card.category):
+          correct_category_turns[turn.card.category] = 0
+    for category, num_correct in correct_category_turns.items():
+      total_for_cat = self.turn_categories()[category]
+      percentage = num_correct / total_for_cat
+      print(f"{category} - {percentage}% correct")
--- jamison/python/flashcards/runner.py
+++ jamison/python/flashcards/runner.py
@@ -25,7 +25,6 @@ def start():
   print(f"****** Game over! ******")
   round.total_correct_percentage()
   print(f"You had {round.number_correct} correct guesses out of {deck_size} for a total score of {round.total_correct_percentage()}.")
-  for card in round.answered_cards:
-    print(f"{card.category()} - {round.correct_by_category(card.category())} correct")
+  round.correct_by_category()
 
 start()
